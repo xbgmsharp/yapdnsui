7cca33ae6b0f110f59c2010c952fac1d445256e9
xbgmsharp <xbgmsharp@gmail.com>
2015-08-25 14:29:52 +0000

    Fix index route

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   if (!req.db) { res.render('', {}); }
-  database.list(req, res, null, function(req, res, json, rows) {
+  database.list(req, res, function(req, res, rows) {
            res.render('', { 'serverlist': rows, 'navmenu': '' });
      });
 });
